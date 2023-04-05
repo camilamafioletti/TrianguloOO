bfebfcb77ca323a9f283403ad35c8930b62f72f8
Camila <camilamafioletti01@gmail.com>
2023-04-05 17:12:08 -0300

    Add project files.

---
+++ TrianguloOO.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TrianguloOO.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();  
+
+            Console.WriteLine("Insira o valor do comprimento dos lados do triângulo ");
+
+            Console.Write("Lado 1: ");
+            triangulo.lado1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Lado 2: ");
+            triangulo.lado2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Lado 3: ");
+            triangulo.lado3 = double.Parse(Console.ReadLine());
+
+            triangulo.CalcularTriangulo();  
+        }
+    }
+}
---
+++ TrianguloOO.ConsoleApp/Triangulo.cs
@@ -0,0 +1,32 @@
+﻿namespace TrianguloOO.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public double lado1 { get; set; }
+        public double lado2 { get; set; }
+        public double lado3 { get; set; }
+
+        public void CalcularTriangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                if (lado1 == lado2 && lado1 == lado3)
+                {
+                    Console.WriteLine("\nÉ um triângulo equilátero");
+                }
+                else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3)
+                {
+                    Console.WriteLine("\nÉ um triângulo escaleno");
+                }
+                else
+                {
+                    Console.WriteLine("\nÉ um triângulo isósceles");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNão é um triângulo");
+            }
+        }
+    }
+}
